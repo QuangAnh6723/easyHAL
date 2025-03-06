3c86b938ff90bd33e3cee25635bff3bbd6b8f561
anh le <lequanganh6723@gmail.com>
2025-03-06 23:26:36 +0700

    init project

---
+++ Core/easyHAL/easyi2c.c
@@ -0,0 +1,14 @@
+/*
+ * easyi2c.c
+ *
+ *  Created on: Mar 6, 2025
+ *      Author: le quang anh
+ */
+
+#include "easyi2c.h"
+#include "main.h"
+
+void I2C_Scan_Bus()
+{
+
+}
---
+++ Core/easyHAL/easyi2c.h
@@ -0,0 +1,15 @@
+/*
+ * easyi2c.h
+ *
+ *  Created on: Mar 6, 2025
+ *      Author: lequa
+ */
+
+#ifndef EASYHAL_EASYI2C_H_
+#define EASYHAL_EASYI2C_H_
+
+
+
+
+
+#endif /* EASYHAL_EASYI2C_H_ */
